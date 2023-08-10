9cacad69de154e73b3488bafe703cba6e896446f
Roman Schwarz <rs@cloudeteer.de>
2023-08-10 21:28:17 +0200

    Keep semicolon as separator

--- zoho.py
+++ zoho.py
@@ -102,7 +102,9 @@ if __name__ == "__main__":
         tags_list = ", ".join(f"{t}" for t in tags)
 
         # Annotations as notes
-        notes = track["annotation"].replace("; ", "\n") if "annotation" in track else ""
+        notes = (
+            track["annotation"].replace("; ", ";\n") if "annotation" in track else ""
+        )
 
         # Get Zoho project and task named based on tags
         project = get_project_and_task(tags)
